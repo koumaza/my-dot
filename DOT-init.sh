e8970c9476657df0e01014995c655d9b2601b7bc
syanhai_yakisoba=\chan! <koumaza--@outlook.com>
2019-12-16 08:24:25 +0000

    url to local

--- DOT-init.sh
+++ DOT-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export URL="https://github.com/koumaza/my-dot/raw/master"
-export INSTALL_PACKAGE=$(wget "$URL/Install.pkg.cfg" >& /dev/null ; cat Install.pkg.cfg|tr "\r" " " ;rm Install.pkg.cfg)
+export INSTALL_PACKAGE=$(cat Install.pkg.cfg|tr "\r" " " ;rm Install.pkg.cfg)
 
 
-export INHERENT_PACKAGE=$(wget "$URL/$DOT-INIT-OS/Inherent.pkg.cfg" >& /dev/null ; cat Inherent.pkg.cfg|tr "\r" " " ;rm Inherent.pkg.cfg)
+
+export INHERENT_PACKAGE=$(cat $DOT-INIT-OS/Inherent.pkg.cfg|tr "\r" " " ;rm Inherent.pkg.cfg)
